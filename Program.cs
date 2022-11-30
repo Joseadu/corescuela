1e54a0675a7967d666e30d9f3a2f3eff3a697ccb
Joseadu <jose.uk94@gmail.com>
2022-11-30 16:01:03 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Escuela
+{
+    public class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+
+        public void Timbrar()
+        {
+            Console.Beep(1000, 1500);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+                miEscuela.nombre = "Platzi";
+                miEscuela.direccion = "Joaquín Turina 14";
+                miEscuela.añoFundacion = 30;
+                miEscuela.Timbrar();
+            
+            Console.WriteLine(miEscuela);
+        }
+    }
+}
